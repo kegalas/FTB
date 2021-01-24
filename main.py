9862ba37bf748ac87f65ff1bf697f5d595205a5d
kegalas <1556223824@qq.com>
2021-01-24 18:14:33 +0800

    0.2.0 update 加入对手球员

--- main.py
+++ main.py
@@ -33,6 +33,9 @@ for i in range(0,11):
 plyrpos = ((200,230),(330,230),(80,380),(200,400),(330,400),\
     (450,380),(80,580),(200,600),(330,600),(450,580),(265,780))
 
+plyrpos2 = ((265,180),(155,180),(375,180),(80,280),(450,280),\
+    (265,365),(155,365),(200,540),(330,540),(375,365),(265,20))
+
 for i in range(0,11):
     allplyr[i].rect.centerx = plyrpos[i][0]
     allplyr[i].rect.centery = plyrpos[i][1]
@@ -41,9 +44,15 @@ for i in range(0,11):
     allplyr[i].img_x = float(allplyr[i].rect.x)
     allplyr[i].img_y = float(allplyr[i].rect.y)
 
-#
+for i in range(0,11):
+    allplyr2[i].rect.centerx = plyrpos2[i][0]
+    allplyr2[i].rect.centery = plyrpos2[i][1]
+    allplyr2[i].img_centerx = float(plyrpos2[i][0])
+    allplyr2[i].img_centery = float(plyrpos2[i][1])
+    allplyr2[i].img_x = float(allplyr2[i].rect.x)
+    allplyr2[i].img_y = float(allplyr2[i].rect.y)
 
-#player1 = Player.Player(screen)
+
 
 class Checkclick():
     def __init__(self):
@@ -55,7 +64,8 @@ while True:
     ftb.mouse_check_event(ifclick)
     for i in range(0,11):
         if ifclick.ifc:
-            allplyr[i].mov(allplyr,i) 
+            allplyr[i].mov(allplyr,allplyr2,i) 
+            allplyr2[i].mov(allplyr,allplyr2,i) 
     
     #ftb.mouse_check_event(player1)
     
@@ -68,6 +78,7 @@ while True:
 
     for i in range(0,11):
         allplyr[i].blitme()
+        allplyr2[i].blitme()
 
     #player1.blitme()                #绘制球员
     pygame.display.update()               #刷新屏幕
--- player.py
+++ player.py
@@ -11,59 +11,6 @@ class Player():
         self.screen = screen
 
         self.localtion = os.path.dirname(os.path.abspath(__file__)) + os.sep+'images'+os.sep+'player-s.png'      #球员图片位置
-        print(self.localtion)
-        self.image = pygame.image.load(self.localtion)
-        self.rect = self.image.get_rect()       #获取球员矩形
-        self.screen_rect = screen.get_rect()
-
-
-        #将球员开始时放在屏幕最下方正中间
-        self.rect.centerx = self.screen_rect.centerx
-        #self.rect.bottom = self.screen_rect.bottom
-        self.rect.centery = self.screen_rect.top
-        #使球员的位置可以储存小数
-        self.img_centerx = float(self.rect.centerx)
-        self.img_centery = float(self.rect.centery)
-        self.img_x = float(self.rect.x)
-        self.img_y = float(self.rect.y)
-        self.moving = False
-
-    def mov(self,allplyr,number):
-        """让球员随着鼠标移动"""
-        self.position = pygame.mouse.get_pos()
-        self.text_intersect = False
-        for i in range(0,11):   #判断是否与其他球员重叠
-            if i==number:
-                continue
-            else:
-                if(self.position[0]>=allplyr[i].img_x and \
-                    self.position[0]<=allplyr[i].img_x+allplyr[i].rect.w\
-                    and self.position[1]>=allplyr[i].img_y and \
-                    self.position[1]<=allplyr[i].img_y+allplyr[i].rect.h):
-                    self.text_intersect = True
-                    break
-
-        if(self.position[0]>=self.img_x and self.position[0]<=self.img_x+self.rect.w\
-            and self.position[1]>=self.img_y and self.position[1]<=self.img_y+self.rect.h\
-            and self.text_intersect==False):    #移动球员到指针处
-            self.img_centerx = self.position[0]
-            self.img_centery = self.position[1]
-            self.rect.centerx = self.img_centerx
-            self.rect.centery = self.img_centery
-            self.img_x = self.rect.x
-            self.img_y = self.rect.y
-        
-    def blitme(self):
-        """绘制球员"""
-        self.screen.blit(self.image,self.rect)
-
-class Player2():            
-    """储存对方球员信息的类"""
-    def __init__(self,screen):
-        """储存一些基本信息"""
-        self.screen = screen
-
-        self.localtion = os.path.dirname(os.path.abspath(__file__)) + os.sep+'images'+os.sep+'player-s.png'      #球员图片位置
         #print(self.localtion)
         self.image = pygame.image.load(self.localtion)
         self.rect = self.image.get_rect()       #获取球员矩形
@@ -81,11 +28,11 @@ class Player2():
         self.img_y = float(self.rect.y)
         self.moving = False
 
-    def mov(self,allplyr,number):
+    def mov(self,allplyr,allplyr2,number):
         """让球员随着鼠标移动"""
         self.position = pygame.mouse.get_pos()
         self.text_intersect = False
-        for i in range(0,11):
+        for i in range(0,11):   #判断是否与1队球员重叠
             if i==number:
                 continue
             else:
@@ -95,10 +42,20 @@ class Player2():
                     self.position[1]<=allplyr[i].img_y+allplyr[i].rect.h):
                     self.text_intersect = True
                     break
+        for i in range(0,11):   #判断是否与2队球员重叠
+            if i==number:
+                continue
+            else:
+                if(self.position[0]>=allplyr2[i].img_x and \
+                    self.position[0]<=allplyr2[i].img_x+allplyr2[i].rect.w\
+                    and self.position[1]>=allplyr2[i].img_y and \
+                    self.position[1]<=allplyr2[i].img_y+allplyr2[i].rect.h):
+                    self.text_intersect = True
+                    break
 
         if(self.position[0]>=self.img_x and self.position[0]<=self.img_x+self.rect.w\
             and self.position[1]>=self.img_y and self.position[1]<=self.img_y+self.rect.h\
-            and self.text_intersect==False):
+            and self.text_intersect==False):    #移动球员到指针处
             self.img_centerx = self.position[0]
             self.img_centery = self.position[1]
             self.rect.centerx = self.img_centerx
@@ -110,3 +67,26 @@ class Player2():
         """绘制球员"""
         self.screen.blit(self.image,self.rect)
 
+class Player2(Player):            
+    """储存对方球员信息的类"""
+    def __init__(self,screen):
+        """储存一些基本信息"""
+        self.screen = screen
+
+        self.localtion = os.path.dirname(os.path.abspath(__file__)) + os.sep+'images'+os.sep+'player-1-s.png'      #球员图片位置
+        #print(self.localtion)
+        self.image = pygame.image.load(self.localtion)
+        self.rect = self.image.get_rect()       #获取球员矩形
+        self.screen_rect = screen.get_rect()
+
+
+        #将球员开始时放在屏幕最下方正中间
+        self.rect.centerx = self.screen_rect.centerx
+        #self.rect.bottom = self.screen_rect.bottom
+        self.rect.centery = self.screen_rect.top
+        #使球员的位置可以储存小数
+        self.img_centerx = float(self.rect.centerx)
+        self.img_centery = float(self.rect.centery)
+        self.img_x = float(self.rect.x)
+        self.img_y = float(self.rect.y)
+        self.moving = False
